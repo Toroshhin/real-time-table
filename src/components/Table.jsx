a6e528ae223cdc6dad1c1ec4a37b53960141fdd2
toroshhin <sergeitoroshin@yandex.ru>
2021-01-23 23:19:51 +0300

    add totalPrice

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,53 +4,55 @@ import { useDispatch } from 'react-redux'
 import editPrice from '../actions/editPrice'
 import editQuantity from '../actions/editQuantity'
 import PropTypes from 'prop-types'
+import TotalPriceRow from './TotalPriceRow'
 
 export default function Table({ rows }) {
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Название</th>
-            <th>Количество</th>
-            <th>Цена за 1</th>
-            <th>Цена (total)</th>
+    <div className='table' >
 
-          </tr>
-        </thead>
-        <tbody>
+      <div className='table-head'>
+        <div className='cell-head'>Количество</div>
+        <div className='cell-head'>Название</div>
+        <div className='cell-head'>Цена за 1</div>
+        <div className='cell-head'>Цена (total)</div>
+      </div>
 
-          <td>
-            {rows.map((row) => (
-              <tr>{row.name}</tr>
-            ))}
-          </td>
-          <td>
-            {rows.map((row) => (
-              <tr className='tr'
-                onClick={() =>
-                  dispatch(editQuantity(row.id, prompt('Введите значение')))
-                } >{row.quantity}</tr>
-            ))}
-          </td>
-          <td>
-            {rows.map((row) => (
-              <tr className='tr'
-                onClick={() =>
-                  dispatch(editPrice(row.id, prompt('Введите значение')))
-                } >{row.priceForOne}</tr>
-            ))}
-          </td>
-          <td>
-            {rows.map((row) => (
-              <tr>{row.priceForOne * row.quantity}</tr>
-            ))}
+      <div className='table-body'>
+        <div className='cell-body'>
+          {rows.map((row) => (
+            <div className='cell'>{row.name}</div>
+          ))}
+        </div>
 
-          </td>
-        </tbody>
-      </table>
+        <div className='cell-body'>
+          {rows.map((row) => (
+            <div className='cell'
+              onClick={() =>
+                dispatch(editQuantity(row.id, prompt('Введите значение')))
+              } >{row.quantity}</div>
+          ))}
+        </div>
+
+        <div className='cell-body'>
+          {rows.map((row) => (
+            <div className='cell'
+              onClick={() =>
+                dispatch(editPrice(row.id, prompt('Введите значение')))
+              } >{row.priceForOne}</div>
+          ))}
+        </div>
+
+        <div className='cell-body'>
+          {rows.map((row) => (
+            <div className='cell' >{row.priceForOne * row.quantity}</div>
+          ))}
+        </div>
+      </div>
+
+      <TotalPriceRow />
+
     </div >
 
   );
