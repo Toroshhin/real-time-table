b5387823ba5ca5af30a563aa7649114e988242db
Toroshhin <sergeitoroshin@yandex.ru>
2021-04-15 23:26:45 +0300

    add socket.io and node server

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Table from './components/Table'
 import { useSelector } from 'react-redux'
+import socket from './socket'
 
 function App() {
-  const rows = useSelector((state) => state)
+  const rows = useSelector(state => state)
   return (
-    <div div className="app" >
+    <div div className='app'>
       <Table rows={rows} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
